92478246bf533581ee4cab6339c19a2db83ccca3
Marlon2103 <marlonprogrammer@gmail.com>
2022-03-02 11:27:57 -0300

    atualizou

--- src/app/model/Postagem.ts
+++ src/app/model/Postagem.ts
@@ -1,11 +1,12 @@
 import { Tema } from "./Tema";
-import { Usuario } from "./Usuario";
+import { User } from "./User";
 
 export class Postagem {
     public id: number
     public titulo: string
     public texto: string
     public data: Date
-    public usuario: Usuario
     public tema: Tema
+    public usuario: User
+    
 }
